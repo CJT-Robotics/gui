00401b0a91993391af5b7c148db936e1e97a04fd
Markus Peltsarszky <149835564+marpelt@users.noreply.github.com>
2023-11-23 00:26:29 +0100

    Adding files

--- barcode_reader/src/barcode_reader.cpp
+++
@@ -1,5 +0,0 @@
-#include <include/barcode_reader/barcode_reader.hpp>
-#include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(barcode_reader::BarcodeReader, nodelet::Nodelet);
